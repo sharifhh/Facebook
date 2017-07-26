fb4e8350bf2ff276bc06d0cb96baff63cfb76dec
sharifhh <shairfh96@gmail.com>
2017-07-26 10:21:14 +0300

    added home activity

---
+++ app/src/main/java/ps/exalt/facebook/SplashActivity.java
@@ -0,0 +1,17 @@
+package ps.exalt.facebook;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sharif on 7/26/2017.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
