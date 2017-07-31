1c1095bf2b68280a3de6036246cbfabadd13908a
sharifhh <shairfh96@gmail.com>
2017-07-31 12:23:58 +0300

    Data sources

---
+++ app/src/main/java/ps/exalt/facebook/Login/LoginViewImpl.java
@@ -0,0 +1,18 @@
+package ps.exalt.facebook.Login;
+
+
+/**
+ * Created by Sharif on 7/31/2017.
+ */
+
+public class LoginViewImpl implements LoginView {
+    @Override
+    public void loginSuccess() {
+
+    }
+
+    @Override
+    public void loginFailed() {
+
+    }
+}
